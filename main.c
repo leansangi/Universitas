3dbcea3b99cc66274a80122b306d73ed4c0bff35
clydeBarrows <clydeBarrows@users.noreply.github.com>
2016-09-06 17:17:19 -0300

    Add files via upload

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <sqlite3.h>
 #include <string.h>
+#include <time.h>
 
 int Menu();
 
@@ -18,11 +19,17 @@ int main (int argc, char **argv) {
 	char auxObservaciones[1024];
 	int auxIntDni;
 	int auxIntMetodo;
+	int legajo;
 	sqlite3 *db;          // Definimos un puntero a la base de datos
 	char *errMsg = 0;     // Variable para el mensaje de error
 	int rc;               // Variable para el retorno de la sentencia
 	sqlite3_stmt *stmt;
 	sqlite3_stmt *result; // Puntero a la respuesta de la consulta
+	int auxLegajo;
+	char auxChDni[1024];
+	
+	
+	
 	rc = sqlite3_open("universitas.db", &db);
 	// Abro la conexión con la base de datos
 	if (rc != SQLITE_OK) {
@@ -126,14 +133,36 @@ int main (int argc, char **argv) {
 			fgets(aluNme, 1024, stdin);
 			printf("Ingrese numero de documento: \n");
 			fgets(aluDni, 20, stdin);
-			printf("Ingrese plan de pago deseado: \n 1 -- 6 cuotas sin interes \n 2 -- 12 cuotas con interes del 10 porciento \n 1 -- 1 pago con 35 porciento de descuento \n");
+			printf("Ingrese plan de pago deseado: \n 1 -- 6 cuotas sin interes \n 2 -- 12 cuotas con interes del 10 porciento \n 3 -- 1 pago con 35 porciento de descuento \n");
 			fgets(auxOpcionLocal, 50, stdin);
 			printf("Ingrese, de ser necesario, una observacion del usuario: \n");
 			fgets(auxObservaciones, 1024, stdin);
 			auxIntDni = atoi(aluDni);
 			auxIntMetodo = atoi(auxOpcionLocal);
-			asprintf(&query, "INSERT INTO alumnos(aluNme, aluDni, metodoPago, costoTotal, observaciones) VALUES ('%s', '%i', '%i', '%i', '%s')", &aluNme
-															, auxIntDni, auxIntMetodo, 25000, &auxObservaciones);
+			
+			rc = sqlite3_prepare(db, "SELECT legajoActual FROM auxiliaryData ", -1, &result, NULL);
+			// Compruebo que no hay error
+			if (rc != SQLITE_OK) {
+				fprintf(stderr, "Error en la consulta: %s.\n", sqlite3_errmsg(db));
+				sqlite3_close(db);
+				return(3);
+			}
+			while ( sqlite3_step(result) == SQLITE_ROW) {
+				auxLegajo = sqlite3_column_int(result,0);
+			}
+			++auxLegajo;
+			
+			asprintf(&query, "INSERT INTO alumnos(aluNme, aluDni, metodoPago, costoTotal, nroCuenta, legajo, observaciones, estadoAlu) VALUES ('%s', '%i', '%i', '%i', '%i', '%i', '%s', 'ACTIVO')", &aluNme
+															, auxIntDni, auxIntMetodo, 25000, auxIntDni, auxLegajo, &auxObservaciones);
+			rc=sqlite3_exec(db, query, NULL, NULL, &errMsg);
+			if (errMsg != NULL) {
+				printf("Error in sqlite3_exec: %s\n", errMsg);
+				sqlite3_free(errMsg);
+			}
+			free(query);
+			getchar();
+			
+			asprintf(&query, "UPDATE auxiliaryData SET legajoActual = %i", auxLegajo);
 			rc=sqlite3_exec(db, query, NULL, NULL, &errMsg);
 			if (errMsg != NULL) {
 				printf("Error in sqlite3_exec: %s\n", errMsg);
@@ -141,6 +170,10 @@ int main (int argc, char **argv) {
 			}
 			getchar();
 			free(query);
+			
+		break;
+		case '6':
+			
 		break;
 		case '8':
 			// Consulta a realizar sobre la tabla.
@@ -153,11 +186,12 @@ int main (int argc, char **argv) {
 			}
 			// Bucle de presentación en pantalla del resultado de la consulta
 			while ( sqlite3_step(result) == SQLITE_ROW) {
-				fprintf(stderr, "Los usuarios actuales son:  ============= ID = %i \n Nombre Alumno = %s \n Dni = %i \n Metodo de Pago = %i \n Costo total del año = %i.\n Observaciones = %s \n"
+				fprintf(stderr, "Los usuarios actuales son:  ============= ID = %i \n Nombre Alumno = %s \n Dni = %i \n Metodo de Pago = %i \n Costo total del anio = %i.\n Numero de Cuenta = %i \n Legajo = %i \n Observaciones = %s \n Estado = %s \n"
 																					, sqlite3_column_int(result, 0)
 																					, sqlite3_column_text(result, 1)
 																					, sqlite3_column_int(result, 2)
-																		, sqlite3_column_int(result, 3), sqlite3_column_int(result, 4), sqlite3_column_text(result, 5));
+																		, sqlite3_column_int(result, 3), sqlite3_column_int(result, 4), sqlite3_column_int(result, 5)
+																		, sqlite3_column_int(result, 6), sqlite3_column_text(result, 7), sqlite3_column_text(result, 8));
 				getchar();
 			}
 		break;
@@ -185,28 +219,16 @@ int Menu()
 		printf("6 -- Cobros adeudados al dia de la fecha\n"); 
 		printf("7 -- Cierre de caja\n"); 
 		printf("8 -- Listar Alumnos Actuales\n");
+		printf("9 -- Dar de baja Alumno\n");
 		printf("0 -- Salir\n"); 
 		fgets(resp, 15, stdin); 
 	}while(resp[0] < '0' && resp[0] > '9'|| resp[0]=="f"); 
 		return resp[0];while(resp!=0);
 }
 
-int letraANum (char* buffersillo[]){
-	char buffer[50];
-	int i, val, len, maxlen;
-	
-	maxlen = atoi( buffersillo[1] );
-	len = strlen(buffer) - 1;  // because buffer includes a newline char
-	if( len > maxlen ){
-		printf( "too long\n" );
-		return 1;
-	}
-	for( i = 0; i < len; ++i ) {
-		if(  !isdigit( buffer[i] ) ){
-			printf( "invalid input\n" );
-			return 1;
-		}
-	}
-	val = atoi(buffer);
-	return (val);
+int generarCuenta(char* dni){
+	int resu;
+	strncat(dni, "04", 20);
+	resu = atoi(dni);
+	return resu;
 }
